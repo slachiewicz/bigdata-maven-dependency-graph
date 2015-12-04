e21b0b61bdf97a855014b7f89a4c16985c06c5e0
Pieter van der Meer <pieter@elucidator.nl>
2015-12-04 17:07:30 +0100

    Added base code for the resolver

---
+++ resolver/src/main/java/nl/ordina/jtechnologies/bigdata/maven/dependency_graph/resolver/dump/FlatDependencyGraphDumper.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2012 Pieter van der Meer (pieter(at)elucidator.nl)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package nl.ordina.jtechnologies.bigdata.maven.dependency_graph.resolver.dump;
+
+import org.sonatype.aether.graph.DependencyNode;
+import org.sonatype.aether.graph.DependencyVisitor;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Dumper
+ */
+public class FlatDependencyGraphDumper implements DependencyVisitor {
+    List<String> nodes = new ArrayList<String>();
+    private DependencyNode root;
+
+    @Override
+    public boolean visitEnter(DependencyNode node) {
+        if (root == null) {
+            root = node;
+            return true;
+        }
+
+        nodes.add(root.getDependency().getArtifact() + " -> " + node.getDependency().getArtifact() + "-> " + "resolved");
+        return true;
+    }
+
+    @Override
+    public boolean visitLeave(DependencyNode node) {
+        return true;
+    }
+
+    public Collection<? extends String> getNodes() {
+        return nodes;
+    }
+}
