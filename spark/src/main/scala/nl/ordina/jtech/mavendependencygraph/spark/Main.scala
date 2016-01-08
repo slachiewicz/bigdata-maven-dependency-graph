5d1d082df5a8182ff0e635aa31d3caef3e510cf1
CasperKoning <casper.e.koning@gmail.com>
2016-01-08 13:53:54 +0100

    spark streaming boiler plate code

--- spark/src/main/scala/nl/ordina/jtech/mavendependencygraph/spark/Main.scala
+++ spark/src/main/scala/nl/ordina/jtech/mavendependencygraph/spark/Main.scala
@@ -1,12 +1,20 @@
 package nl.ordina.jtech.mavendependencygraph.spark
 
-import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.{StreamingContext, Seconds}
 
 class Main {
 
   def main(args: Array[String]): Unit = {
-    
-    val sc = new SparkContext()
-    
+    val host = ???
+    val port = ???
+    val ssc = new StreamingContext(new SparkConf, Seconds(30))
+    val dstream = ssc.socketTextStream(host,port)
+
+    //TODO logic
+
+    ssc.start()
+    ssc.awaitTermination()
+    ssc.stop()
   }
 }
