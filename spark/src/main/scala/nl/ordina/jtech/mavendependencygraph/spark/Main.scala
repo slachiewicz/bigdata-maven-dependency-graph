c2004388c23a45f2fac846f69d1ad5d20ed6c486
Mendel van 't Riet <mendel.van.t.riet@ordina.nl>
2016-01-08 13:41:46 +0100

    added spark dependency

--- spark/src/main/scala/nl/ordina/jtech/mavendependencygraph/spark/Main.scala
+++ spark/src/main/scala/nl/ordina/jtech/mavendependencygraph/spark/Main.scala
@@ -1,9 +1,12 @@
 package nl.ordina.jtech.mavendependencygraph.spark
 
+import org.apache.spark.SparkContext
+
 class Main {
 
   def main(args: Array[String]): Unit = {
     
+    val sc = new SparkContext()
     
   }
 }
