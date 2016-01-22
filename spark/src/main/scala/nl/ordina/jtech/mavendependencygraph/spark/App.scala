1ebc64f5cd4602a1630965b5c00892ee17a1f46e
CasperKoning <casper.e.koning@gmail.com>
2016-01-22 09:56:11 +0100

    input check

--- spark/src/main/scala/nl/ordina/jtech/mavendependencygraph/spark/App.scala
+++ spark/src/main/scala/nl/ordina/jtech/mavendependencygraph/spark/App.scala
@@ -6,11 +6,18 @@ import org.apache.spark.streaming.{StreamingContext, Seconds}
 object App {
 
   def main(args: Array[String]): Unit = {
+    if(args.length != 2) {
+      println(
+        """
+          |Please provide the following arguments:
+          | args0: host
+          | args1: port
+        """.stripMargin)
+    }
     val host = args(0)
     val port = args(1).toInt
     val ssc = new StreamingContext(new SparkConf, Seconds(30))
     val dstream = ssc.socketTextStream(host,port)
-
 
     dstream.map({ record => 
       val group, artifact, version = record
