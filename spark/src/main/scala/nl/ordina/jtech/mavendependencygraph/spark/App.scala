ca1b5b8fb2ff37e91480ce1b0c604ff01dc1290d
CasperKoning <casper.e.koning@gmail.com>
2016-01-22 13:49:19 +0100

    stream boilerplate 2

--- spark/src/main/scala/nl/ordina/jtech/mavendependencygraph/spark/App.scala
+++ spark/src/main/scala/nl/ordina/jtech/mavendependencygraph/spark/App.scala
@@ -1,12 +1,14 @@
 package nl.ordina.jtech.mavendependencygraph.spark
 
+import nl.ordina.jtech.mavendependencygraph.model.DependencyGraph
 import org.apache.spark.SparkConf
+import org.apache.spark.rdd.RDD
 import org.apache.spark.streaming.{StreamingContext, Seconds}
 
 object App {
 
   def main(args: Array[String]): Unit = {
-    if(args.length != 2) {
+    if (args.length != 2) {
       println(
         """
           |Please provide the following arguments:
@@ -16,17 +18,30 @@ object App {
     }
     val host = args(0)
     val port = args(1).toInt
-    val ssc = new StreamingContext(new SparkConf, Seconds(30))
-    val dstream = ssc.socketTextStream(host,port)
+    val conf = new SparkConf().setMaster("local[2]").setAppName("maven-streaming")
+    val ssc = new StreamingContext(conf, Seconds(30))
 
-    dstream.map({ record => 
-      val group, artifact, version = record
-      ((group, artifact, version), Array("Hello","World"))
-    }).print()
-    
+    val dstream = ssc.socketTextStream(host, port)
+    dstream.map(parseMavenEntry)
+           .map(resolveSubGraph)
+           .foreachRDD(sendGraphToNeo _)
 
     ssc.start()
     ssc.awaitTermination()
     ssc.stop()
   }
+
+  def parseMavenEntry(record: String): MavenEntry = {
+    val splits = record.split(";")
+    val groupId = splits(0)
+    val artifactId = splits(1)
+    val version = splits(2)
+    val packaging = splits(3)
+    val classifier = splits(4)
+    MavenEntry(groupId,artifactId,version,packaging,classifier)
+  }
+
+  def resolveSubGraph(mavenEntry: MavenEntry): DependencyGraph = ??? //TODO: Call resolver
+
+  def sendGraphToNeo(graph: RDD[DependencyGraph]): Unit = ??? //TODO: Send to Neo
 }
