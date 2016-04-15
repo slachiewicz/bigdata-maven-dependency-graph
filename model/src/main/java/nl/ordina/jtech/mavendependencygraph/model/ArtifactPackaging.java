0a0fae0da3c3a24f8565cf46b39bebd3d4f73bf8
Silfen66 <gerry.noij@ordina.nl>
2016-04-15 10:14:36 +0200

    extra package and scope items added

--- model/src/main/java/nl/ordina/jtech/mavendependencygraph/model/ArtifactPackaging.java
+++ model/src/main/java/nl/ordina/jtech/mavendependencygraph/model/ArtifactPackaging.java
@@ -1,10 +1,11 @@
 package nl.ordina.jtech.mavendependencygraph.model;
 
+// The current core packaging values are: pom, jar, maven-plugin, ejb, war, ear, rar, par.
 public enum ArtifactPackaging {
-    Jar, War, Ear, Pom;
+    Jar, War, Ear, Pom, Ejb, Rar, Par, Maven_Plugin, Unknown;
 
     public static ArtifactPackaging parseFromString(String packaging) {
-        switch (packaging) {
+        switch (packaging.toLowerCase()) {
             case "jar":
                 return ArtifactPackaging.Jar;
             case "war":
@@ -13,8 +14,16 @@ public enum ArtifactPackaging {
                 return ArtifactPackaging.Ear;
             case "pom":
                 return ArtifactPackaging.Pom;
+            case "ejb":
+                return ArtifactPackaging.Ejb;
+            case "rar":
+                return ArtifactPackaging.Rar;
+            case "par":
+                return ArtifactPackaging.Par;
+            case "maven-plugin":
+                return ArtifactPackaging.Maven_Plugin;
             default:
-                return null;
+                return ArtifactPackaging.Unknown;
         }
     }
 }
