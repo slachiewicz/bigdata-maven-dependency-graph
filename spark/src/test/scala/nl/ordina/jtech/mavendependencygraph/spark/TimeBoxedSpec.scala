7c672e45325a37420b0ef05f2c6861034cd748d4
Casper Koning <casper.e.koning@gmail.com>
2016-06-12 12:19:52 +0200

    Add extra test that tests happy flow

--- spark/src/test/scala/nl/ordina/jtech/mavendependencygraph/spark/TimeBoxedSpec.scala
+++ spark/src/test/scala/nl/ordina/jtech/mavendependencygraph/spark/TimeBoxedSpec.scala
@@ -9,6 +9,13 @@ class TimeBoxedSpec extends FunSuite {
   implicit val timeout = 10 milliseconds
   val durationThatTimesOut = 20L
 
+  test("TimeBoxedOperations that do not exceed their duration should just return the result") {
+    val result = TimeBoxed {
+      true
+    }.get(1 seconds)
+    assert(result)
+  }
+
   test("TimeBoxedOperations that exceed their duration should fail with a timeout") {
     intercept[TimeoutException](TimeBoxed {
       Thread.sleep(durationThatTimesOut)
